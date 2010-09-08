d85e5bd7795ff910f60f7e54e9369754238246f9
otto.allmendinger@googlemail.com <none@none>
2010-09-08 15:44:09 +0200

    updated grooveshark-desktop, gs version checker script

--- grooveshark-desktop/check-version.sh
+++ grooveshark-desktop/check-version.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+OPT_MAIL=0
+OPT_DOWNLOAD=0
+
+while getopts :md option; do
+    case ${option} in
+        m)  OPT_MAIL=1 ;;
+        d)  OPT_DOWNLOAD=1 ;;
+    esac
+done
+
+echo "mail: ${OPT_MAIL}"
+
+
 if !(which cronkite > /dev/null); then
     echo "get the cronkite"
     exit 1
@@ -8,14 +22,27 @@ aur_data=($(cronkite -info grooveshark-desktop))
 aur_version=${aur_data[3]%-*}
 
 air_xmlns="http://ns.adobe.com/air/framework/update/description/1.0"
-grooveshark_version_url="http://preview.grooveshark.com/desktop/version.php"
-latest_version=$(curl -so- ${grooveshark_version_url} | \
-    xml sel -N x=${air_xmlns} -t -v "/x:update/x:version")
+gs_version_url="http://preview.grooveshark.com/desktop/version.php"
+gs_version_file="/tmp/gs_info.xml"
+curl -s -o${gs_version_file} ${gs_version_url}
+gs_latest_version=$(xml sel -N x=${air_xmlns} -t -v "/x:update/x:version" \
+    $gs_version_file)
+gs_download_url=$(xml sel -N x=${air_xmlns} -t -v "/x:update/x:url" \
+    $gs_version_file)
 
-if [ ${latest_version} != ${aur_version} ]; then
-    echo "new version - mailing message"
-    echo "New grooveshark version: ${latest_version}" | \
-        mail -s "grooveshark-desktop update" otto
+if [ "${gs_latest_version}" != "${aur_version}" ]; then
+    msg="New grooveshark version ${latest_version}"
+    echo ${msg}
+
+    if [ ! -z "${OPT_DOWNLOAD}" ]; then 
+        wget "${gs_download_url}"
+    fi
+
+    if [ "${OPT_MAIL}" != "0" ]; then
+        echo "mailing message..."
+        echo "${msg}" | mail -s "grooveshark-desktop update" otto
+    fi
+
 else
     echo "aur version is latest version"
 fi
